d70acf98f1a32024ab735c02dd28f063683f7d30
raymondmar61 <raym61@hotmail.com>
2020-05-26 17:08:37 -0700

    Python and JSON straight forward and together

--- jsonpython.py
+++ jsonpython.py
@@ -61,3 +61,145 @@ print(type(jsonfile[1]["peoplearray"])) #print <class 'list'>
 print(jsonfile[1]["peoplearray"][2]) #print {'name': 'Sara', 'age': 25}
 print(type(jsonfile[1]["peoplearray"][2])) #print <class 'dict'>
 print(jsonfile[1]["peoplearray"][2]["age"]) #print 25
+
+#Python Tutorial Working with JSON Data using the json Module
+import json
+#Convert string to Python object.
+#people is the key.  The value of the array is two objects.  Each objects has four keys name, phone, emails, haslicense.  Why peoplestring needs triple single-quotes? 
+peoplestring = '''{"people":[{"name":"John Smith", "phone":"615-555-7164", "emails":["johnsmith@bogusemail.com","john.smith@work-place.com"], "haslicense":true}, {"name":"Jane Doe", "phone":"560-555-5153", "emails":null, "haslicense":true}]}'''
+loadtopython = json.loads(peoplestring)
+print(loadtopython) #print {'people': [{'name': 'John Smith', 'phone': '615-555-7164', 'emails': ['johnsmith@bogusemail.com', 'john.smith@work-place.com'], 'haslicense': True}, {'name': 'Jane Doe', 'phone': '560-555-5153', 'emails': None, 'haslicense': True}]}
+print(type(loadtopython)) #print <class 'dict'>.  Loading json object to Python it becomes a dictionary by default.
+#https://docs.python.org/3/library/jsonhtml#encoders-and-decoders a list of json and what Python converts to.  Another example json array to Python it becomes a list.
+print(type(loadtopython["people"])) #print <class 'list'>
+for sinceitsalist in loadtopython["people"]:
+	print(type(sinceitsalist)) #print <class 'dict'>\n <class 'dict'>
+	print(sinceitsalist) #print {'name': 'John Smith', 'phone': '615-555-7164', 'emails': ['johnsmith@bogusemail.com', 'john.smith@work-place.com'], 'haslicense': True}\n {'name': 'Jane Doe', 'phone': '560-555-5153', 'emails': None, 'haslicense': True}
+	print(sinceitsalist["name"]) #print John Smith\n Jane Smith
+for deletephone in loadtopython["people"]:
+	del deletephone["phone"]
+print(peoplestring) #print {"people":[{"name":"John Smith", "phone":"615-555-7164", "emails":["johnsmith@bogusemail.com","john.smith@work-place.com"], "haslicense":true}, {"name":"Jane Doe", "phone":"560-555-5153", "emails":null, "haslicense":true}]}
+removephone = json.dumps(peoplestring) #convert Python object to string
+print(type(removephone)) #print <class 'str'>
+print(removephone) #print "{\"people\":[{\"name\":\"John Smith\", \"phone\":\"615-555-7164\", \"emails\":[\"johnsmith@bogusemail.com\",\"john.smith@work-place.com\"], \"haslicense\":true}, {\"name\":\"Jane Doe\", \"phone\":\"560-555-5153\", \"emails\":null, \"haslicense\":true}]}"
+removephonecorrect= json.dumps(loadtopython)
+print(removephonecorrect) #print {"people": [{"name": "John Smith", "emails": ["johnsmith@bogusemail.com", "john.smith@work-place.com"], "haslicense": true}, {"name": "Jane Doe", "emails": null, "haslicense": true}]}
+removephonecorrectindent= json.dumps(loadtopython, indent=2, sort_keys=True)
+print(removephonecorrectindent)
+'''
+{
+  "people": [
+    {
+      "emails": [
+        "johnsmith@bogusemail.com",
+        "john.smith@work-place.com"
+      ],
+      "haslicense": true,
+      "name": "John Smith"
+    },
+    {
+      "emails": null,
+      "haslicense": true,
+      "name": "Jane Doe"
+    }
+  ]
+}
+'''
+#Load JSON files and Python objects and write objects back to JSON files
+#First, a self-discovered lesson on triple quotes.
+#Add triple quotes because TypeError: the JSON object must be str, bytes or bytearray, not 'dict'
+statesareacodes = '''{"states":[{"name":"Alabama","abbreviation":"AL","area_codes":["205","251","256","334","938"]},{"name":"Alaska","abbreviation":"AK","area_codes":["907"]},{"name":"Arizona","abbreviation":"AZ","area_codes":["480","520","602","623","928"]},{"name":"Arkansas","abbreviation":"AR","area_codes":["479","501","870"]},{"name":"California","abbreviation":"CA","area_codes":["209","213","310","323","408","415","424","442","510","530","559","562","619","626","628","650","657","661","669","707","714","747","760","805","818","831","858","909","916","925","949","951"]},{"name":"Colorado","abbreviation":"CO","area_codes":["303","719","720","970"]},{"name":"Connecticut","abbreviation":"CT","area_codes":["203","475","860","959"]}'''
+loadstatestopython = json.loads(statesareacodes)
+print(loadstatestopython) #print {'states': [{'name': 'Alabama', 'abbreviation': 'AL', 'area_codes': ['205', '251', '256', '334', '938']}, {'name': 'Alaska', 'abbreviation': 'AK', 'area_codes': ['907']}, {'name': 'Arizona', 'abbreviation': 'AZ', 'area_codes': ['480', '520', '602', '623', '928']}, . . .
+print(type(loadstatestopython)) #print <class 'dict'>
+#Load JSON files and Python objects and write objects back to JSON files
+with open("states.json") as fileobject:
+	pythonobjectdata = json.load(fileobject)
+print(pythonobjectdata) #print {'states': [{'name': 'Alabama', 'abbreviation': 'AL', 'area_codes': ['205', '251', '256', '334', '938']}, {'name': 'Alaska', 'abbreviation': 'AK', 'area_codes': ['907']}, {'name': 'Arizona', 'abbreviation': 'AZ', 'area_codes': ['480', '520', '602', '623', '928']}, . . .
+print(type(pythonobjectdata)) #print <class 'dict'>
+for state in pythonobjectdata["states"]:
+	print(state) #print {'name': 'Alabama', 'abbreviation': 'AL', 'area_codes': ['205', '251', '256', '334', '938']}\n {'name': 'Alaska', 'abbreviation': 'AK', 'area_codes': ['907']}\n . . .
+for state in pythonobjectdata["states"]:
+	print(state["name"]+", "+state["abbreviation"]) #print Alabama, AL\n Alaska, AK\n . . .
+#delete area codes from pythonobjectdata
+for deleteareacodes in pythonobjectdata["states"]:
+	del deleteareacodes["area_codes"]
+#The dump method converts the data to JSON file.  The dumps method converts the data to JSON string.
+stringstates = json.dumps(pythonobjectdata)
+print(stringstates) #print {"states": [{"name": "Alabama", "abbreviation": "AL"}, {"name": "Alaska", "abbreviation": "AK"}, {"name": "Arizona", "abbreviation": "AZ"}, . . .
+#Create new JSON file newstates.json
+with open("newstates.json","w") as fileobject:
+	json.dump(pythonobjectdata, fileobject, indent=2)
+'''
+#Another example.  Download JSON data from an API.  Converts dollars to another currency at Yahoo.  Unfortunately, the link doesn't work.  I do my best.  Comment the example.
+from urllib.request import urlopen
+with urlopen("https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json") as response:
+	source = response.read()
+print(source) #prints content of website as a string
+data = json.loads(source) #convert source as a string to a Python object as the variable data
+print(len(data["list"]["resources"])) #count the number of resources contents inside the list list
+conversionratesdictionary = dict()
+for eachforeigncurrencyconversion in data["list"]["resources"]:
+	print(eachforeigncurrencyconversion)
+	foreigncountry = eachforeigncurrencyconversion["resource"]["fields"]["name"]
+	usdconversionnumber = eachforeigncurrencyconversion["resource"]["fields"]["price"]
+	print(foreigncountry, price) #print USD/ALL 113.230003 #RM:  it's one example.
+	conversionratesdictionary[foreigncountry] = usdconversionnumber
+print(conversionratesdictionary["USD/EUR"]) #print 0.846500
+readjsonbetter = json.dumps(data, indent=2)
+print(readjsonbetter)
+'''
+
+#What Is JSON JSON Crash Course
+#There are five key-value pairs.  Objects are wrapped by curly braces; a combination of key value pairs.  Keys are strings.  Values are numbers, strings, objects, or arrays.  Arrays are wrapped by square brackets.  Arrays are multiple values for the same key.  In particular, streetaddress, city, state, and zip are an object for address.  phonenumber is an array with two objects home and fax phone numbers.
+example = {"firstname":"John", "lastname":"Smith", "age":25, "address":{"streetaddress":"21 2nd Street", "city":"New York", "state":"NY", "zip":"10021"}, "phonenumber":[{"type":"home", "number":"212 555-1234"}, {"type":"fax","number":"646 555-4567"}]}
+print(example) #print {'firstname': 'John', 'lastname': 'Smith', 'age': 25, 'address': {'streetaddress': '21 2nd Street', 'city': 'New York', 'state': 'NY', 'zip': '10021'}, 'phonenumber': [{'type': 'home', 'number': '212 555-1234'}, {'type': 'fax', 'number': '646 555-4567'}]}
+print(type(example)) #print <class 'dict'>
+examplestring = '''{"firstname":"John", "lastname":"Smith", "age":25, "address":{"streetaddress":"21 2nd Street", "city":"New York", "state":"NY", "zip":"10021"}, "phonenumber":[{"type":"home", "number":"212 555-1234"}, {"type":"fax","number":"646 555-4567"}]}'''
+print(examplestring) #print {"firstname":"John", "lastname":"Smith", "age":25, "address":{"streetaddress":"21 2nd Street", "city":"New York", "state":"NY", "zip":"10021"}, "phonenumber":[{"type":"home", "number":"212 555-1234"}, {"type":"fax","number":"646 555-4567"}]}
+print(type(examplestring)) #print <class 'str'>
+stringtojson = json.loads(examplestring)
+print(stringtojson) #print {'firstname': 'John', 'lastname': 'Smith', 'age': 25, 'address': {'streetaddress': '21 2nd Street', 'city': 'New York', 'state': 'NY', 'zip': '10021'}, 'phonenumber': [{'type': 'home', 'number': '212 555-1234'}, {'type': 'fax', 'number': '646 555-4567'}]}
+print(type(stringtojson)) #print <class 'dict'>
+print(stringtojson["address"]) #print {'streetaddress': '21 2nd Street', 'city': 'New York', 'state': 'NY', 'zip': '10021'}
+print(stringtojson["address"]["city"]) #print New York
+print(stringtojson["phonenumber"]) #print [{'type': 'home', 'number': '212 555-1234'}, {'type': 'fax', 'number': '646 555-4567'}]
+print(type(stringtojson["phonenumber"])) #print <class 'list'>
+#print(stringtojson["phonenumber"]["type"]) #print TypeError: list indices must be integers or slices, not str
+for eachtype in stringtojson["phonenumber"]:
+	print(eachtype["type"]) #print home\n fax
+
+#Python Tutorial for Beginners 43 - Working With JSON Data in Python
+awhatever = {"name":"max", "age":22, "marks": [90, 50, 80, 40], "pass":True}
+print(type(awhatever)) #print <class 'dict'>
+print(json.dumps(awhatever)) #print {"name": "max", "age": 22, "marks": [90, 50, 80, 40], "pass": true}
+print(json.dumps(awhatever, separators=(". ","="))) #print {"name"="max". "age"=22. "marks"=[90. 50. 80. 40]. "pass"=true}
+print(json.dumps({"name":"max", "age":22})) #print {"name": "max", "age": 22}
+print(json.dumps(["1", "2"])) #print ["1", "2"]
+print(json.dumps(("1", "2"))) #print ["1", "2"]
+print(json.dumps("Hello World")) #print "Hello World"
+print(json.dumps(100)) #print 100
+print(type(json.dumps(100))) #print <class 'str'>
+print(json.dumps(15.56)) #print 15.56
+print(json.dumps(False)) #print false
+print(json.dumps(True)) #print true
+print(json.dumps(None)) #print null
+with open("saveasjsonfile.json","w") as fileobject:
+	fileobject.write(json.dumps(awhatever))
+fileobject.close()
+with open("saveasjsonfile.json","r") as fileobject2:
+	print(fileobject2.read()) #print {"name": "max", "age": 22, "marks": [90, 50, 80, 40], "pass": true}
+	print(type(fileobject2.read())) #print <class 'str'>
+	#print(json.loads(fileobject2.read())) #print json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+	givemethestring = fileobject2.read()
+	print(givemethestring) #print null
+	jsonstring = fileobject2.read()
+	#jsonvalue = json.loads(jsonstring)
+	#print(type(jsonvalue)) #print json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+#The tutorial doesn't work.  A prior tutorial works.
+openjsonjsonfile = open("saveasjsonfile.json","r")
+jsonfile = json.load(openjsonjsonfile)
+print(jsonfile) #print {'name': 'max', 'age': 22, 'marks': [90, 50, 80, 40], 'pass': True}
+print(type(jsonfile)) #print <class 'dict'>
+openjsonjsonfile.close()
+print(jsonfile["name"]) #print max
